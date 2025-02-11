aab2b1172119e3b0e813795ecfa20d4a3220a3c9
Gubaydullin Zakir <zakirmullabaev@gmail.com>
2025-02-11 14:58:21 +0300

    add style.css file

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+
+
+
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+
+
